3c3d062a33535d47bbe92e9d691049c548d939af
Tishida <archersparadox.100@gmail.com>
2024-01-14 17:12:50 +0900

    tabs途中まで

---
+++ components/ui/tabs.tsx
@@ -0,0 +1,31 @@
+import { Box, Tabs as RadixTabs, Text } from '@radix-ui/themes';
+
+type TabsProps = {
+  selectedLabel: string;
+  labels: string[];
+  onChange: (value: string) => void;
+};
+
+const Tabs = ({ selectedLabel, labels, onChange }: TabsProps) => {
+  return (
+    <RadixTabs.Root defaultValue={selectedLabel}>
+      <RadixTabs.List>
+        {labels.map((label, i) => (
+          <RadixTabs.Trigger key={`${label}${i}`} value={label}>
+            {label}
+          </RadixTabs.Trigger>
+        ))}
+      </RadixTabs.List>
+
+      <Box px="4" pt="2" pb="2">
+        {labels.map((label, i) => (
+          <RadixTabs.Content key={`${label}${i}`} value={label}>
+            <Text size="2">{label}</Text>
+          </RadixTabs.Content>
+        ))}
+      </Box>
+    </RadixTabs.Root>
+  );
+};
+
+export default Tabs;
